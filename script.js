ef328537efba6381f2b22dbd8a874d086a0c0e65
eriaht <thomasehaire@gmail.com>
2024-03-18 16:16:30 -0400

    added call to main

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const { parse } = require("node-html-parser");
 
 /*
@@ -11,10 +12,15 @@ const getHTML = async (vocabUrl) => {
     const res = await fetch(vocabUrl);
     const html = await res.text();
 
-    fs.writeFile("./vocab.html", html, "utf-8", (error) => {
-      if (error) throw error;
-      else console.log("Successfully wrote to vocab.html");
-    });
+    fs.writeFileSync(
+      path.join(__dirname, "vocab.html"),
+      html,
+      "utf-8",
+      (error) => {
+        if (error) throw error;
+        else console.log("Successfully wrote to vocab.html");
+      }
+    );
   } catch (error) {
     console.error(error);
   }
@@ -24,15 +30,18 @@ const getHTML = async (vocabUrl) => {
   Parse the provided HTML file, then store all the vocabulary words in a text file
   called words.txt. This file is stored in the directory the script is executed in.
 */
-const parseHTML = (htmlFile) => {
+const parseHTML = () => {
   // Read html
-  const html = fs.readFileSync(htmlFile, "utf-8");
+  const html = fs.readFileSync("./vocab.html", {
+    encoding: "utf-8",
+    flags: "r",
+  });
   // Parse html
   const root = parse(html);
   // Select all words
   const words = root.querySelectorAll(".word");
   // Create a write stream
-  let logger = fs.createWriteStream("words.txt", {
+  let logger = fs.createWriteStream(path.join(__dirname, "words.txt"), {
     flags: "w",
   });
   // Write each word to the text file
@@ -42,3 +51,25 @@ const parseHTML = (htmlFile) => {
   // Close the write stream
   logger.end();
 };
+
+const main = async (args) => {
+  try {
+    // Get HTML
+    console.log("Getting html from vocabulary.com...");
+    await getHTML(args[2]);
+
+    // Parse HTMl and store it in a text file
+    console.log("Parsing html for words...");
+    parseHTML();
+
+    // Log success message
+    console.log("Successfully stored vocab words in words.txt");
+  } catch (error) {
+    // Log error message
+    console.error(
+      `Failed to scrape words from vocabulary.com\n${error.message}`
+    );
+  }
+};
+
+main(process.argv);
